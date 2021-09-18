d397cdd4615ba5f5c78bd103242760472d68369b
Krishna Rowter <krishna.rowter@gmail.com>
2021-09-18 15:30:57 +0700

    add transcription as content script

---
+++ scripts/transcription.js
@@ -0,0 +1,37 @@
+const transcription = document.createElement("span");
+document.body.appendChild(transcription);
+
+const initialOptions = {
+  lang: "en-US",
+  continuous: true,
+  interimResults: true
+};
+
+let result = "",
+  recognition = {},
+  offset = 90, // max characters in transcription result before being cleared
+  offsetIndex = 0;
+
+chrome.runtime.onMessage.addListener(() => {
+  try {
+    recognition = new webkitSpeechRecognition();
+  } catch {
+    console.log("Please use a chrome-based browser on laptop/desktop");
+  }
+
+  Object.assign(recognition, initialOptions);
+
+  recognition.onresult = function (event) {
+    if (transcription.textContent.length > offset) {
+      offsetIndex++;
+    }
+
+    const result = [...event.results]
+      .map(result => result[0].transcript)
+      .join(" ");
+
+    transcription.textContent = result.slice(offsetIndex * offset);
+  };
+
+  recognition.start();
+});
